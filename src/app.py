15b279e31d20a49759827f9a10ac5653bc20f2f2
YauheniKr <smilodon2@yandex.ru>
2021-12-28 16:06:52 +0300

    переопределил эндпойнты добавил необходимые импорты

--- src/app.py
+++ src/app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_restful import Api
 
-from src.services.role import role_route
+from src.api.role_api import RoleCreate, RoleGetUpdateDelete, RolesGet, RoleUserCreateDelete, CheckUserRole
 
 app = Flask(__name__)
-app.register_blueprint(role_route)
+api = Api(app)
+api.add_resource(RoleGetUpdateDelete, '/api/v1/auth/role/<string:role_id>')
+api.add_resource(RoleCreate, '/api/v1/auth/role/')
+api.add_resource(RolesGet, '/api/v1/auth/roles/')
+api.add_resource(RoleUserCreateDelete, '/api/v1/auth/role/user/')
+api.add_resource(CheckUserRole, '/api/v1/auth/role/user/status')
 
 if __name__ == '__main__':
     app.run()
