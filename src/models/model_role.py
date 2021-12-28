f3bad2bb75ea41d7f8b8b3d099e8dac9228c7df7
YauheniKr <smilodon2@yandex.ru>
2021-12-28 15:51:21 +0300

    добавил метод to_json для модели Role добавил модель Role_User добавил колонку с описанием Роли перенес описание связи в модель Role_User

--- src/models/model_role.py
+++ src/models/model_role.py
@@ -1,8 +1,8 @@
 import uuid
 
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import validates
 
 from src.models.model_base import ModelBase
 
@@ -13,7 +13,21 @@ class Role(ModelBase):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
     role_name = Column(String, unique=True, nullable=False)
     role_weight = Column(Integer, unique=True, nullable=False)
-    role_user = relationship('User', back_populates="user")
+    description = Column(String(255))
 
     def __repr__(self):
         return f'{self.role_name}'
+
+    def to_json(self):
+        return {
+            'id': str(self.id),
+            'role_name': self.role_name,
+            'role_weight': self.role_weight,
+        }
+
+class Role_User(ModelBase):
+    __tablename__ = 'role_user'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = Column("user_id", UUID(as_uuid=True), ForeignKey("user.id"))
+    role_id = Column("role_id", UUID(as_uuid=True), ForeignKey("role.id"))
