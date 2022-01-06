a23658b883f4e990a5f0bf1b8d5904419ef81aa6
YauheniKr <smilodon2@yandex.ru>
2022-01-06 21:21:41 +0300

    модели, скопированы из основного проекта

---
+++ tests/functional/models/model_role.py
@@ -0,0 +1,27 @@
+import uuid
+
+from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+
+from tests.functional.models.model_base import ModelBase
+
+
+class Role(ModelBase):
+    __tablename__ = 'role'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    role_name = Column(String, unique=True, nullable=False)
+    role_weight = Column(Integer, unique=True, nullable=False)
+    description = Column(String(255))
+
+    def __repr__(self):
+        return f'{self.role_name}'
+
+
+
+class RoleUser(ModelBase):
+    __tablename__ = 'role_user'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = Column("user_id", UUID(as_uuid=True), ForeignKey("user.id"))
+    role_id = Column("role_id", UUID(as_uuid=True), ForeignKey("role.id"))
---
+++ tests/functional/models/model_user.py
@@ -0,0 +1,32 @@
+from sqlalchemy.sql import func
+import uuid
+
+from sqlalchemy import Column, String, DateTime, Text, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import backref, relationship
+
+from tests.functional.models.model_base import ModelBase
+
+
+class User(ModelBase):
+    __tablename__ = 'user'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    username = Column(String(50), unique=True)
+    password = Column(String(128))
+    email = Column(String(70), unique=True, nullable=False)
+
+    role = relationship("Role", secondary="role_user", backref=backref("users", lazy="dynamic"), )
+
+
+class AuthHistory(ModelBase):
+    __tablename__ = "auth_history"
+
+    id = Column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False
+    )
+    user_id = Column("user_id", UUID(as_uuid=True), ForeignKey("user.id"))
+    timestamp = Column(DateTime,  server_default=func.now())
+    user_agent = Column(Text, nullable=False)
+    ip_address = Column(String(20))
+    device = Column(Text)
