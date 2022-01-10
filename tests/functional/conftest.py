d28655a4aec4ec572a61a3bd22517db0e48980d0
YauheniKr <smilodon2@yandex.ru>
2022-01-10 20:29:25 +0300

    добавил необходимые импорты добавил функцию flask_app необходимую для генерации токенов с помощью flask_jwt_extended добавил фикстуру redis_session при записи информации опользователях в БД пароли хэшируются добавил фикстуру генерации токенов

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,8 +1,13 @@
-import logging
-import pytest
+from datetime import datetime
 
-from tests.functional.settings import test_settings
+import pytest
+from flask import Flask
+from flask_jwt_extended import decode_token, JWTManager, create_access_token, create_refresh_token
+from werkzeug.security import generate_password_hash
+
+from tests.functional.settings import test_settings as settings
 from tests.functional.utils.global_init import create_session
+from tests.functional.utils.redis_storage import redis_conn
 
 
 @pytest.fixture(scope='session')
@@ -10,6 +15,21 @@ def postgres_session():
     session = create_session()
     yield session
     session.close()
+
+
+def flask_app():
+    app = Flask(__name__)
+    JWTManager(app)
+    app.config.update({"JWT_SECRET_KEY": settings.SECRET_KEY, "JWT_ACCESS_TOKEN_EXPIRES": settings.ACCESS_EXPIRES,
+                       "JWT_REFRESH_TOKEN_EXPIRES": settings.REFRESH_EXPIRES})
+    return app.app_context().push()
+
+
+@pytest.fixture(scope='function')
+def redis_session():
+    session = redis_conn
+    yield session
+    session.flushall()
 
 
 @pytest.fixture(scope='session')
@@ -59,12 +79,12 @@ def users(postgres_session):
     from tests.functional.models.model_user import User
     user_test_data_1 = {
         'username': 'user1',
-        'password': '123',
+        'password': generate_password_hash('123', method='sha256'),
         'email': 'fake@yamdb.ru'
     }
     user_test_data_2 = {
         'username': 'user2',
-        'password': '123',
+        'password': generate_password_hash('123', method='sha256'),
         'email': 'fake@yamdb.com'
     }
     users = [user_test_data_1, user_test_data_2]
@@ -77,3 +97,19 @@ def users(postgres_session):
     postgres_session.query(User).delete()
     postgres_session.commit()
 
+
+@pytest.fixture(scope='function')
+def tokens(users, redis_session):
+    user_id = str(users[0].id)
+    flask_app()
+    access_token = create_access_token(identity=user_id)
+    refresh_token = create_refresh_token(identity=user_id)
+    token = {
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    }
+    decoded_token = decode_token(refresh_token)
+    jti = decoded_token["jti"]
+    expires = datetime.fromtimestamp(decoded_token["exp"]) - datetime.now()
+    redis_session.setex(name=str(users[0].id), value=jti, time=expires)
+    return token
