aa33b1b31c95624d85ad9b26dcba10ea1363043c
YauheniKr <smilodon2@yandex.ru>
2022-01-10 17:54:09 +0300

    удалил лишние данные добавил данные для создания токенов

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -1,20 +1,13 @@
-from pydantic import BaseSettings
-from pydantic.parse import Path
+import hashlib
+from datetime import timedelta
 
-BASE_DIR = Path(__file__).resolve().parent
-TESTDATA_DIR = Path(*[BASE_DIR / 'testdata' / 'data'])
-SCHEMA_DIR = Path(*[BASE_DIR / 'testdata' / 'schema'])
-UTILS_DIR = [BASE_DIR / 'utils']
+from pydantic import BaseSettings
 
 
 class TestSettings(BaseSettings):
     # Настройки Redis
     REDIS_HOST: str = 'redis'
     REDIS_PORT: int = 6379
-
-    # Настройки Elasticsearch
-    ELASTIC_HOST: str = 'postgres'
-    ELASTIC_PORT: int = 5432
 
     # Настройки сервиса
     SERVICE_HOST: str = 'auth_api'
@@ -26,6 +19,11 @@ class TestSettings(BaseSettings):
     POSTGRES_USER: str = 'postgres'
     POSTGRES_PASSWORD: str = 'postgres'
     POSTGRES_DB: str = 'movies'
+
+    # Настройки Flask
+    SECRET_KEY = hashlib.md5('super_secret'.encode()).hexdigest()
+    ACCESS_EXPIRES = timedelta(minutes=60)
+    REFRESH_EXPIRES = timedelta(days=15)
 
     class Config:
         case_sensitive = True
