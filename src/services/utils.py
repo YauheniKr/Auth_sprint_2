cdcdf05ca154f74aea618529df7f69d1c433ce93
YauheniKr <smilodon2@yandex.ru>
2022-01-15 13:26:14 +0300

    добавил функцию для пагинации вывода записей

--- src/services/utils.py
+++ src/services/utils.py
@@ -2,6 +2,7 @@ from functools import wraps
 
 from flask import make_response
 from flask_jwt_extended import verify_jwt_in_request, get_jwt
+from flask_restful import abort
 
 
 def admin_required():
@@ -18,3 +19,30 @@ def admin_required():
         return decorator
 
     return wrapper
+
+
+def get_paginated_list(results, url, page, limit):
+    page = int(page)
+    limit = int(limit)
+    count = len(results)
+    if count < page or limit < 0:
+        abort(404)
+    # make response
+    obj = {}
+    obj['page'] = page
+    obj['limit'] = limit
+    obj['count'] = count
+    # make URLs
+    # make previous url
+    if page == 1:
+        obj['previous'] = ""
+    else:
+        obj['previous'] = f'{url}?page={page-1}&limit={limit}'
+    # make next url
+    if page * limit > count:
+        obj['next'] = ""
+    else:
+        obj['next'] = f'{url}?page={page+1}&limit={limit}'
+    # finally extract result according to bounds
+    obj['results'] = results[limit*(page - 1):page * limit]
+    return obj
