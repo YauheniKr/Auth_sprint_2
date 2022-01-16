face4220c35b833af5dec88afeeb74c2596d4a9e
YauheniKr <smilodon2@yandex.ru>
2022-01-16 23:46:04 +0300

    функция проверки доступности сервера API

---
+++ tests/functional/utils/wait_for_api.py
@@ -0,0 +1,36 @@
+import logging
+from time import sleep
+import requests
+import http
+
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+URL = 'http://auth_api:8000/apidocs/'
+
+
+def make_request():
+    status = requests.head(URL)
+    return status
+
+
+def check_api_availability():
+    while True:
+        try:
+            make_request()
+            if make_request().status_code == http.HTTPStatus.OK:
+                logger.info("API server running")
+                exit(0)
+            else:
+                logger.error("API is not available. Sleep for 10 sec")
+                sleep(10)
+        except requests.exceptions.ConnectionError:
+            logger.error("API is not available. Sleep for 10 sec")
+            sleep(10)
+        else:
+            exit(1)
+
+
+if __name__ == '__main__':
+    check_api_availability()
