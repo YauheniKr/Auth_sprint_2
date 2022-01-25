3f68096cb1d214fbe6eaefd7d21140168cef86e9
yauheni <smilodon2@yandex.ru>
2022-01-26 02:37:53 +0300

    добавил считывание настроек из .env файла

--- src/core/config.py
+++ src/core/config.py
@@ -1,10 +1,12 @@
 import hashlib
 from datetime import timedelta
 
+from dotenv import load_dotenv
 from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
+    env_file = load_dotenv()
     # Настройки Redis
     REDIS_HOST: str = 'redis'
     REDIS_PORT: int = 6379
@@ -21,11 +23,9 @@ class Settings(BaseSettings):
     ACCESS_EXPIRES = timedelta(minutes=60)
     REFRESH_EXPIRES = timedelta(days=15)
 
-    #Настройки Yandex
+    # Настройки Yandex
     YANDEX_ID: str
-    YANDEX_PASWORD: str
-
-
+    YANDEX_PASSWORD: str
 
     class Config:
         case_sensitive = True
