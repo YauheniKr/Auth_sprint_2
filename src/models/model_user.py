713e6ef46e90e7de26ef793d6e7b19e9924a2a58
yauheni <smilodon2@yandex.ru>
2022-01-26 02:37:24 +0300

    добавил модель определения параметров соц аккаунта

--- src/models/model_user.py
+++ src/models/model_user.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column, DateTime, ForeignKey, String, Text
+from sqlalchemy import Column, DateTime, ForeignKey, String, Text, UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import backref, relationship
 from sqlalchemy.sql import func
@@ -33,3 +33,18 @@ class AuthHistory(ModelBase):
     user_agent = Column(Text, nullable=False)
     ip_address = Column(String(20))
     device = Column(Text)
+
+
+class SocialAccount(ModelBase):
+    __tablename__ = 'social_account'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('user.id'), nullable=False)
+    user = relationship(User, backref=backref('social_accounts', lazy=True))
+    social_id = Column(Text, nullable=False)
+    social_name = Column(Text, nullable=False)
+
+    __table_args__ = (UniqueConstraint('social_id', 'social_name', name='social_pk'),)
+
+    def __repr__(self):
+        return f'<SocialAccount {self.social_name}:{self.user_id}>'
