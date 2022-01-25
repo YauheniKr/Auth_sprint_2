9d67e880c001bdeb64114a7626ab8c99e67e545d
yauheni <smilodon2@yandex.ru>
2022-01-26 02:08:38 +0300

    вынес генерацию токена в отдельную функцию

--- src/services/utils.py
+++ src/services/utils.py
@@ -1,8 +1,10 @@
 from functools import wraps
 
 from flask import make_response
-from flask_jwt_extended import get_jwt, verify_jwt_in_request
+from flask_jwt_extended import get_jwt, verify_jwt_in_request, create_access_token, create_refresh_token, decode_token
 from flask_restful import abort
+
+from src.services.redis_service import RedisTokenStorage
 
 
 def admin_required():
@@ -46,3 +48,19 @@ def get_paginated_list(results, url, page, limit):
     # finally extract result according to bounds
     obj['results'] = results[limit * (page - 1):page * limit]
     return obj
+
+
+def generate_tokens(user_id, is_admin: bool):
+    additional_claims = {}
+    if is_admin:
+        additional_claims = {"is_administrator": True}
+    access_token = create_access_token(identity=user_id, additional_claims=additional_claims)
+    refresh_token = create_refresh_token(identity=user_id, additional_claims=additional_claims)
+    token = {
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    }
+    redis_service = RedisTokenStorage()
+    refresh_token = decode_token(refresh_token)
+    redis_service.add_token_to_database(refresh_token)
+    return token
